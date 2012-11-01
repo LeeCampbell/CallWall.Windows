9b3760281b32d50dd0a4df224fe8dfc0c772a934
Lee Campbell <Lee.Ryan.Campbell@gmail.com>
2012-11-01 09:30:30 +0000

    Added CallWall.Core and its unit test project

---
+++ src/CallWall.Core.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+
+namespace CallWall.Core.UnitTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
