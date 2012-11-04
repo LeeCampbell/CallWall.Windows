3f9a1b95d4a5bc27592d6d9215a03e0b01bbb477
Lee Campbell <Lee.Ryan.Campbell@gmail.com>
2012-11-04 13:24:16 +0000

    Adding comments to ILoggerFactory

--- src/CallWall.Core/ILoggerFactory.cs
+++ src/CallWall.Core/ILoggerFactory.cs
@@ -1,7 +1,14 @@
 namespace CallWall
 {
+    /// <summary>
+    /// Factory for creating <see cref="ILogger"/> instances.
+    /// </summary>
     public interface ILoggerFactory
     {
+        /// <summary>
+        /// Factory method to create an <see cref="ILogger"/> instance.
+        /// </summary>
+        /// <returns>Returns an instance of an <see cref="ILogger"/> for the calling type.</returns>
         ILogger CreateLogger();
     }
 }
