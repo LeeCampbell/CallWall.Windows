14baaef14bcae3a5917480ad6b7ef01fe1ce7d00
Lee Campbell <Lee.Ryan.Campbell@gmail.com>
2012-11-06 23:47:36 +0000

    Removed duplicate code

--- src/CallWall.UnitTests/Properties/AssemblyInfo.cs
+++ src/CallWall.UnitTests/Properties/AssemblyInfo.cs
@@ -1,18 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CallWall.UnitTests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CallWall.UnitTests")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyProduct("CallWall")]
+[assembly: AssemblyCompany("Campbell Consultancy London Limited")]
+[assembly: AssemblyCopyright("Copyright © Campbell Consultancy London Limited 2012")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
