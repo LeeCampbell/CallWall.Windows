11aeaf867cbec90e4e4633cc968c5aa170bb0951
Lee Campbell <Lee.Ryan.Campbell@gmail.com>
2012-11-06 19:19:29 +0000

    Adding comments to Logging

--- src/CallWall.Core/LoggerExtensions.cs
+++ src/CallWall.Core/LoggerExtensions.cs
@@ -7,6 +7,13 @@ namespace CallWall
 {
     public static class LoggerExtensions
     {
+        /// <summary>
+        /// Logs a message with an exception as Fatal.
+        /// </summary>
+        /// <param name="logger">The instance of a logger to log with</param>
+        /// <param name="exception">The related <see cref="Exception"/> for the message</param>
+        /// <param name="format">The message as a string format</param>
+        /// <param name="args">The arguments for the message</param>
         [StringFormatMethod("format")]
         public static void Fatal(this ILogger logger, Exception exception, string format, params object[] args)
         {
@@ -14,6 +21,12 @@ namespace CallWall
             var formattedMessage = string.Format(CultureInfo.CurrentCulture, format, args);
             logger.Write(LogLevel.Fatal, formattedMessage, exception);
         }
+        /// <summary>
+        /// Logs a message with an exception as Fatal.
+        /// </summary>
+        /// <param name="logger">The instance of a logger to log with</param>
+        /// <param name="format">The message as a string format</param>
+        /// <param name="args">The arguments for the message</param>
         [StringFormatMethod("format")]
         public static void Fatal(this ILogger logger, string format, params object[] args)
         {
@@ -21,6 +34,13 @@ namespace CallWall
             logger.Fatal(null, format, args);
         }
 
+        /// <summary>
+        /// Logs a message with an exception as Error.
+        /// </summary>
+        /// <param name="logger">The instance of a logger to log with</param>
+        /// <param name="exception">The related <see cref="Exception"/> for the message</param>
+        /// <param name="format">The message as a string format</param>
+        /// <param name="args">The arguments for the message</param>
         [StringFormatMethod("format")]
         public static void Error(this ILogger logger, Exception exception, string format, params object[] args)
         {
@@ -28,6 +48,12 @@ namespace CallWall
             var formattedMessage = string.Format(CultureInfo.CurrentCulture, format, args);
             logger.Write(LogLevel.Error, formattedMessage, exception);
         }
+        /// <summary>
+        /// Logs a message with an exception as Error.
+        /// </summary>
+        /// <param name="logger">The instance of a logger to log with</param>
+        /// <param name="format">The message as a string format</param>
+        /// <param name="args">The arguments for the message</param>
         [StringFormatMethod("format")]
         public static void Error(this ILogger logger, string format, params object[] args)
         {
@@ -35,6 +61,13 @@ namespace CallWall
             logger.Error(null, format, args);
         }
 
+        /// <summary>
+        /// Logs a message with an exception as Info.
+        /// </summary>
+        /// <param name="logger">The instance of a logger to log with</param>
+        /// <param name="exception">The related <see cref="Exception"/> for the message</param>
+        /// <param name="format">The message as a string format</param>
+        /// <param name="args">The arguments for the message</param>
         [StringFormatMethod("format")]
         public static void Info(this ILogger logger, Exception exception, string format, params object[] args)
         {
@@ -42,6 +75,12 @@ namespace CallWall
             var formattedMessage = string.Format(CultureInfo.CurrentCulture, format, args);
             logger.Write(LogLevel.Info, formattedMessage, exception);
         }
+        /// <summary>
+        /// Logs a message with an exception as Info.
+        /// </summary>
+        /// <param name="logger">The instance of a logger to log with</param>
+        /// <param name="format">The message as a string format</param>
+        /// <param name="args">The arguments for the message</param>
         [StringFormatMethod("format")]
         public static void Info(this ILogger logger, string format, params object[] args)
         {
@@ -49,6 +88,13 @@ namespace CallWall
             logger.Info(null, format, args);
         }
 
+        /// <summary>
+        /// Logs a message with an exception as Warn.
+        /// </summary>
+        /// <param name="logger">The instance of a logger to log with</param>
+        /// <param name="exception">The related <see cref="Exception"/> for the message</param>
+        /// <param name="format">The message as a string format</param>
+        /// <param name="args">The arguments for the message</param>
         [StringFormatMethod("format")]
         public static void Warn(this ILogger logger, Exception exception, string format, params object[] args)
         {
@@ -56,6 +102,12 @@ namespace CallWall
             var formattedMessage = string.Format(CultureInfo.CurrentCulture, format, args);
             logger.Write(LogLevel.Warn, formattedMessage, exception);
         }
+        /// <summary>
+        /// Logs a message with an exception as Warn.
+        /// </summary>
+        /// <param name="logger">The instance of a logger to log with</param>
+        /// <param name="format">The message as a string format</param>
+        /// <param name="args">The arguments for the message</param>
         [StringFormatMethod("format")]
         public static void Warn(this ILogger logger, string format, params object[] args)
         {
@@ -63,6 +115,13 @@ namespace CallWall
             logger.Warn(null, format, args);
         }
 
+        /// <summary>
+        /// Logs a message with an exception as Debug.
+        /// </summary>
+        /// <param name="logger">The instance of a logger to log with</param>
+        /// <param name="exception">The related <see cref="Exception"/> for the message</param>
+        /// <param name="format">The message as a string format</param>
+        /// <param name="args">The arguments for the message</param>
         [StringFormatMethod("format")]
         public static void Debug(this ILogger logger, Exception exception, string format, params object[] args)
         {
@@ -70,6 +129,12 @@ namespace CallWall
             var formattedMessage = string.Format(CultureInfo.CurrentCulture, format, args);
             logger.Write(LogLevel.Debug, formattedMessage, exception);
         }
+        /// <summary>
+        /// Logs a message as Debug.
+        /// </summary>
+        /// <param name="logger">The instance of a logger to log with</param>
+        /// <param name="format">The message as a string format</param>
+        /// <param name="args">The arguments for the message</param>
         [StringFormatMethod("format")]
         public static void Debug(this ILogger logger, string format, params object[] args)
         {
@@ -77,6 +142,13 @@ namespace CallWall
             logger.Debug(null, format, args);
         }
 
+        /// <summary>
+        /// Logs a message with an exception as Trace, the second lowest level.
+        /// </summary>
+        /// <param name="logger">The instance of a logger to log with</param>
+        /// <param name="exception">The related <see cref="Exception"/> for the message</param>
+        /// <param name="format">The message as a string format</param>
+        /// <param name="args">The arguments for the message</param>
         [StringFormatMethod("format")]
         public static void Trace(this ILogger logger, Exception exception, string format, params object[] args)
         {
@@ -84,6 +156,13 @@ namespace CallWall
             var formattedMessage = string.Format(CultureInfo.CurrentCulture, format, args);
             logger.Write(LogLevel.Trace, formattedMessage, exception);
         }
+        
+        /// <summary>
+        /// Logs a message as Trace, the second lowest level.
+        /// </summary>
+        /// <param name="logger">The instance of a logger to log with</param>
+        /// <param name="format">The message as a string format</param>
+        /// <param name="args">The arguments for the message</param>
         [StringFormatMethod("format")]
         public static void Trace(this ILogger logger, string format, params object[] args)
         {
@@ -92,7 +171,7 @@ namespace CallWall
         }
 
         /// <summary>
-        /// Logs messages as Verbose, the lowest level.
+        /// Logs a message with an exception as Verbose, the lowest level.
         /// </summary>
         /// <param name="logger">The instance of a logger to log with</param>
         /// <param name="exception">The related <see cref="Exception"/> for the message</param>
@@ -105,6 +184,13 @@ namespace CallWall
             var formattedMessage = string.Format(CultureInfo.CurrentCulture, format, args);
             logger.Write(LogLevel.Verbose, formattedMessage, exception);
         }
+
+        /// <summary>
+        /// Logs a message as Verbose, the lowest level.
+        /// </summary>
+        /// <param name="logger">The instance of a logger to log with</param>
+        /// <param name="format">The message as a string format</param>
+        /// <param name="args">The arguments for the message</param>
         [StringFormatMethod("format")]
         public static void Verbose(this ILogger logger, string format, params object[] args)
         {
@@ -117,8 +203,28 @@ namespace CallWall
         /// Ensure the method being logged is not in-lined by the compiler/jitter with the
         /// [MethodImpl(MethodImplOptions.NoInlining)] attribute.
         /// </summary>
-        /// <param name="logger"></param>
-        /// <param name="args"></param>
+        /// <param name="logger">The instance of a logger to log with</param>
+        /// <param name="args">The arguments passed to the method</param>
+        /// <remarks>
+        /// The <see cref="MethodEntry"/> logging method is useful for logging that a method has 
+        /// been entered and also capturing the arguments of the method in the log.
+        /// <example>
+        /// In this example we log a method entry and the arguments.
+        /// <code>
+        /// <![CDATA[
+        /// [MethodImpl(MethodImplOptions.NoInlining)]
+        /// public void MyLoggedMethod(string s, DateTime dateTime)
+        /// {
+        ///     _logger.MethodEntry(s, dateTime);
+        ///     //Method body goes here...
+        /// }
+        /// 
+        /// ]]>
+        /// </code>
+        /// The output may look something like this
+        /// 2012-01-31 12:00 [UI] DEBUG MyLoggedType.MyLoggedMethod(A, 31/12/2001 13:45:27)
+        /// </example>
+        /// </remarks>
         public static void MethodEntry(this ILogger logger, params object[] args)
         {
             if (logger == null) throw new ArgumentNullException("logger");
