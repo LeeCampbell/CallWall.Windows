8852a2d63800719ad2bc7705a0bb0810861b354d
Lee Campbell <Lee.Ryan.Campbell@gmail.com>
2013-01-18 21:34:42 +0000

    Further formatting of the Contact widget

--- src/CallWall/Settings/Accounts/IAccountSettingsModel.cs
+++ src/CallWall/Settings/Accounts/IAccountSettingsModel.cs
@@ -11,7 +11,7 @@ namespace CallWall.Settings.Accounts
 
         public bool RequiresSetup
         {
-            get { return true; }
+            get { return false; }
         }
 
         #endregion
