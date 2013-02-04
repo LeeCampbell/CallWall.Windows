2e63d0b912ba3b72b192d3d043c15d1bc0e6ea68
Lee Campbell <Lee.Ryan.Campbell@gmail.com>
2013-02-04 22:19:02 +0000

    Correcting tests

--- src/CallWall.Google.UnitTests/Providers/GoogleContactQueryProviderTests.cs
+++ src/CallWall.Google.UnitTests/Providers/GoogleContactQueryProviderTests.cs
@@ -1,9 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Reactive;
 using System.Reactive.Linq;
 using CallWall.Contract;
-using CallWall.Contract.Contact;
 using CallWall.Google.Authorization;
 using CallWall.Google.Providers;
 using CallWall.Web;
@@ -19,30 +18,39 @@ namespace CallWall.Google.UnitTests.Providers
         private StubHttpClient _httpClient;
         private Mock<IGoogleContactProfileTranslator> _translatorMock;
         private Mock<ILogger> _loggerStub;
+        private Mock<ILoggerFactory> _logFactoryStub;
 
         [SetUp]
         public virtual void Setup()
         {
             _authorizationMock = new Mock<IGoogleAuthorization>();
-            _httpClient = new StubHttpClient();
             _translatorMock = new Mock<IGoogleContactProfileTranslator>();
-            var logFactoryStub = new Mock<ILoggerFactory>();
+            _logFactoryStub = new Mock<ILoggerFactory>();
             _loggerStub = new Mock<ILogger>();
-            logFactoryStub.Setup(lf => lf.CreateLogger()).Returns(_loggerStub.Object);
-            _sut = new GoogleContactQueryProvider(_authorizationMock.Object, _httpClient, _translatorMock.Object, logFactoryStub.Object);
+            _logFactoryStub.Setup(lf => lf.CreateLogger()).Returns(_loggerStub.Object);
+            
         }
 
         [TestFixture]
         public sealed class When_loading_ContactProfile : Given_a_constructed_GoogleContactQueryProvider
         {
-            //Should Create a request from the Authorization code, pass to WebClient & return translated response.
+            //Should Create a request from the Authorization code, 
+            //  -->pass to WebClient 
+            //  -->Translate
+            //  -->request groups from webclient
+            //  -->pass groups response and tranlated contact to translator for enrichment of groups
+            //  -->return the enriched contact.
 
             #region Setup
+            private const string _contactsUri = @"https://www.google.com/m8/feeds/contacts/default/full";
+            private const string _groupsUri = @"https://www.google.com/m8/feeds/groups/default/full";
 
             private IProfile _profile;
             private string _accessToken = "The access token";
-            private string _webResponse = "The web response";
+            private string _contactWebResponse = "The contact response";
+            private string _groupsWebResponse = "The groups response";
             private Mock<IGoogleContactProfile> _contactProfileMock;
+            private Mock<IGoogleContactProfile> _enrichedProfileMock;
 
             public override void Setup()
             {
@@ -50,10 +58,26 @@ namespace CallWall.Google.UnitTests.Providers
                 _authorizationMock.Setup(a => a.RequestAccessToken())
                     .Returns(Observable.Return(_accessToken));
                 _contactProfileMock = new Mock<IGoogleContactProfile>();
-                _translatorMock.Setup(t => t.Translate(_webResponse, _accessToken))
+                _enrichedProfileMock = new Mock<IGoogleContactProfile>();
+                _translatorMock.Setup(t => t.Translate(_contactWebResponse, _accessToken))
                     .Returns(_contactProfileMock.Object);
+                _translatorMock.Setup(t => t.AddTags(_contactProfileMock.Object, _groupsWebResponse))
+                    .Returns(_enrichedProfileMock.Object);
                 _profile = CreateProfile();
-                _httpClient.Response = _webResponse;
+
+                _httpClient = new StubHttpClient(request=>
+                                                     {
+                                                         if(request.EndPointUrl == _contactsUri)
+                                                         {
+                                                             return _contactWebResponse;
+                                                         }
+                                                         if(request.EndPointUrl == _groupsUri)
+                                                         {
+                                                             return _groupsWebResponse;
+                                                         }
+                                                         return null;
+                                                     });
+                _sut = new GoogleContactQueryProvider(_authorizationMock.Object, _httpClient, _translatorMock.Object, _logFactoryStub.Object);
             }
 
             private static IProfile CreateProfile()
@@ -71,32 +95,33 @@ namespace CallWall.Google.UnitTests.Providers
             #endregion
 
             [Test]
-            public void Should_call_webClient_with_GoogleContactURI()
+            public void Should_call_webClient_first_with_GoogleContactURI()
             {
                 _sut.LoadContact(_profile).Subscribe();
 
-                Assert.AreEqual(@"https://www.google.com/m8/feeds/contacts/default/full", _httpClient.LastRequest.EndPointUrl);
+                
+                Assert.AreEqual(_contactsUri, _httpClient.Requests[0].EndPointUrl);
             }
             [Test]
-            public void Should_call_webClient_with_AccessToken()
+            public void Should_call_webClient_first_with_AccessToken()
             {
                 _sut.LoadContact(_profile).Subscribe();
 
-                Assert.AreEqual(_accessToken, _httpClient.LastRequest.QueryStringParameters["access_token"]);
+                Assert.AreEqual(_accessToken, _httpClient.Requests[0].QueryStringParameters["access_token"]);
             }
             [Test]
-            public void Should_call_webClient_with_Identifier_value()
+            public void Should_call_webClient_first_with_Identifier_value()
             {
                 _sut.LoadContact(_profile).Subscribe();
 
-                Assert.AreEqual(_profile.Identifiers.Single().Value, _httpClient.LastRequest.QueryStringParameters["q"]);
+                Assert.AreEqual(_profile.Identifiers.Single().Value, _httpClient.Requests[0].QueryStringParameters["q"]);
             }
             [Test]
-            public void Should_call_webClient_request_version_3_of_the_contract()
+            public void Should_call_webClient_first_with_version_3_of_the_contract()
             {
                 _sut.LoadContact(_profile).Subscribe();
 
-                Assert.AreEqual("3.0", _httpClient.LastRequest.Headers["GData-Version"]);
+                Assert.AreEqual("3.0", _httpClient.Requests[0].Headers["GData-Version"]);
             }
 
             [Test]
@@ -108,23 +133,52 @@ namespace CallWall.Google.UnitTests.Providers
             }
 
             [Test]
+            public void Should_call_webClient_second_with_GoogleContactGroupUri()
+            {
+                _sut.LoadContact(_profile).Subscribe();
+
+                Assert.AreEqual(_groupsUri, _httpClient.Requests[1].EndPointUrl);
+            }
+            [Test]
+            public void Should_call_webClient_second_with_AccessToken()
+            {
+                _sut.LoadContact(_profile).Subscribe();
+
+                Assert.AreEqual(_accessToken, _httpClient.Requests[1].QueryStringParameters["access_token"]);
+            }
+            [Test]
+            public void Should_call_webClient_second_with_version_3_of_the_contract()
+            {
+                _sut.LoadContact(_profile).Subscribe();
+
+                Assert.AreEqual("3.0", _httpClient.Requests[1].Headers["GData-Version"]);
+            }
+
+            [Test]
             public void Should_return_translator_result()
             {
                 var actual = _sut.LoadContact(_profile).First();
 
-                Assert.AreSame(_contactProfileMock.Object, actual);
+                Assert.AreSame(_enrichedProfileMock.Object, actual);
             }
         }
 
         private sealed class StubHttpClient : IHttpClient
         {
-            public string Response { get; set; }
-            public HttpRequestParameters LastRequest { get; private set; }
+            private readonly Func<HttpRequestParameters, string> _responder;
+
+            public StubHttpClient(Func<HttpRequestParameters, string> responder)
+            {
+                _responder = responder;
+                Requests = new List<HttpRequestParameters>();
+            }
+
+            public List<HttpRequestParameters> Requests { get; private set; }
 
             public IObservable<string> GetResponse(HttpRequestParameters request)
             {
-                LastRequest = request;
-                return Observable.Return(Response);
+                Requests.Add(request);
+                return Observable.Return(_responder(request));
             }
         }
     }
