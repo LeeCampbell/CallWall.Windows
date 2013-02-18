f8fc1c1265009cb0ef6b77abd0bbea636bc3b282
Lee Campbell <Lee.Ryan.Campbell@gmail.com>
2013-02-18 23:13:04 +0000

    adding unit tests for GoogleAuthorization

--- src/CallWall.Google.UnitTests/Authorization/GoogleAuthorizationFixture.cs
+++ src/CallWall.Google.UnitTests/Authorization/GoogleAuthorizationFixture.cs
@@ -1,29 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reactive;
-using System.Reactive.Disposables;
-using System.Reactive.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CallWall.Google.Authorization;
+﻿using CallWall.Google.Authorization;
 using CallWall.Web;
 using Microsoft.Reactive.Testing;
 using Moq;
 using NUnit.Framework;
+using System;
+using System.Reactive;
+using System.Reactive.Linq;
 
 // ReSharper disable InconsistentNaming
 namespace CallWall.Google.UnitTests.Authorization
 {
     public abstract class Given_a_newly_constructed_GoogleAuthorization
     {
+        #region Setup guff
+
         private GoogleAuthorization _sut;
         private Mock<IPersonalizationSettings> _localStoreMock;
         private Mock<IHttpClient> _httpClientMock;
         private Mock<ILogger> _loggerMock;
 
         private Given_a_newly_constructed_GoogleAuthorization()
-        { }
+        {
+        }
 
         [SetUp]
         public virtual void SetUp()
@@ -36,11 +34,13 @@ namespace CallWall.Google.UnitTests.Authorization
             _sut = new GoogleAuthorization(_localStoreMock.Object, _httpClientMock.Object, logFactory.Object);
         }
 
+        #endregion
+
         [TestFixture]
-        public sealed class When_requesting_AccessToken : Given_a_newly_constructed_GoogleAuthorization
+        public sealed class Given_no_registered_GoogleAuthorization_callback : Given_a_newly_constructed_GoogleAuthorization
         {
             [Test]
-            public void Should_OnError_if_no_callback_has_been_registered()
+            public void Should_OnError_RequestForAccess()
             {
                 var observer = new TestScheduler().CreateObserver<string>();
                 _sut.AvailableResourceScopes[0].IsEnabled = true;
@@ -48,8 +48,30 @@ namespace CallWall.Google.UnitTests.Authorization
                 Assert.AreEqual(1, observer.Messages.Count);
                 Assert.AreEqual(NotificationKind.OnError, observer.Messages[0].Value.Kind);
                 Assert.IsInstanceOf<InvalidOperationException>(observer.Messages[0].Value.Exception);
-                Assert.AreEqual("No call-back has been registered via the RegisterAuthorizationCallback method", observer.Messages[0].Value.Exception.Message);
+                Assert.AreEqual("No call-back has been registered via the RegisterAuthorizationCallback method",
+                                observer.Messages[0].Value.Exception.Message);
             }
+        }
+
+        public abstract class Given_a_registered_GoogleAuthorization : Given_a_newly_constructed_GoogleAuthorization
+        {
+            #region Setup guff
+
+            private Given_a_registered_GoogleAuthorization()
+            {
+            }
+
+            protected abstract RequestAuthorizationCode CreateCallback();
+
+            public override void SetUp()
+            {
+                base.SetUp();
+                var callback = CreateCallback();
+                _sut.RegisterAuthorizationCallback(callback);
+                _sut.AvailableResourceScopes[0].IsEnabled = true;   //Set at least one Resource to enabled to allow nominal tests to pass.
+            }
+
+            #endregion
 
             [Test]
             public void Should_OnError_if_no_Resources_have_been_selected()
@@ -57,49 +79,90 @@ namespace CallWall.Google.UnitTests.Authorization
                 var observer = new TestScheduler().CreateObserver<string>();
                 RequestAuthorizationCode callback = uri => Observable.Empty<string>();
                 _sut.RegisterAuthorizationCallback(callback);
-
+                foreach (var resourceScope in _sut.AvailableResourceScopes)
+                {
+                    resourceScope.IsEnabled = false;
+                }
                 _sut.RequestAccessToken().Subscribe(observer);
 
                 Assert.AreEqual(1, observer.Messages.Count);
                 Assert.AreEqual(NotificationKind.OnError, observer.Messages[0].Value.Kind);
                 Assert.IsInstanceOf<InvalidOperationException>(observer.Messages[0].Value.Exception);
-                Assert.AreEqual("No resources have been enabled.",observer.Messages[0].Value.Exception.Message);
+                Assert.AreEqual("No resources have been enabled.", observer.Messages[0].Value.Exception.Message);
             }
 
-            [Test]
-            public void Should_call_the_registered_callback()
+            [TestFixture]
+            public sealed class When_calling_authorization_callback_with_empty_response : Given_a_registered_GoogleAuthorization
             {
-                var wasCalledAndSubscribedTo = false;
-                RequestAuthorizationCode callback = uri => Observable.Create<string>(
-                    o =>
-                    {
-                        wasCalledAndSubscribedTo = true;
-                        return Disposable.Empty;
-                    });
-                _sut.RegisterAuthorizationCallback(callback);
-                _sut.AvailableResourceScopes[0].IsEnabled = true;
-                _sut.RequestAccessToken().Subscribe();
+                protected override RequestAuthorizationCode CreateCallback()
+                {
+                    RequestAuthorizationCode callback = uri => Observable.Empty<string>();
+                    return callback;
+                }
 
-                Assert.IsTrue(wasCalledAndSubscribedTo);
+                [Test]
+                public void Should_set_status_as_not_authorized()
+                {
+                    _sut.RequestAccessToken().Subscribe();
+                    Assert.IsFalse(_sut.Status.IsAuthorized);
+                }
+
+                [Test]
+                public void Should_set_status_as_not_processing()
+                {
+                    _sut.RequestAccessToken().Subscribe();
+                    Assert.IsFalse(_sut.Status.IsProcessing);
+                }
             }
 
-            //Should validate that there are some resources enabled.
+            [TestFixture]
+            public sealed class When_calling_authorization_callback_with_error_response : Given_a_registered_GoogleAuthorization
+            {
+                private Exception _expectedException;
+
+                public override void SetUp()
+                {
+                    base.SetUp();
+                    _expectedException = new Exception("Fail");
+                }
+
+                protected override RequestAuthorizationCode CreateCallback()
+                {
+                    RequestAuthorizationCode callback = uri => Observable.Throw<string>(_expectedException);
+                    return callback;
+                }
+
+                [Test]
+                public void Should_set_status_as_not_authorized()
+                {
+                    _sut.RequestAccessToken().Subscribe(i => { }, ex => { });
+                    Assert.IsFalse(_sut.Status.IsAuthorized);
+                }
+
+                [Test]
+                public void Should_set_status_as_not_processing()
+                {
+                    _sut.RequestAccessToken().Subscribe(i=>{},ex=> { });
+                    Assert.IsFalse(_sut.Status.IsProcessing);
+                }
+
+                [Test]
+                public void Should_pass_through_error()
+                {
+                    Exception actual = null;
+                    _sut.RequestAccessToken().Subscribe(_ => { }, ex => { actual = ex; });
+                    Assert.AreSame(_expectedException, actual);
+                }
+            }
+
             //when multiple concurrent requests for access token are made should only call call-back once
-            //
         }
 
-        //When calling Login/Authorize callback
-            //Should ?? on empty response
-            //Should ?? on error response
-
-
+        //Given an Authorized_GoogleAuthoization
+        //  When requesting access token
+        //  when requesting access token for modified resources
+        //  when multiple concurrent requests for access token are made
+        //Given a lapsed GoogleAuthoization
     }
-
-    //Given an Authorized_GoogleAuthoization
-    //  When requesting access token
-    //  when requesting access token for modified resources
-    //  when multiple concurrent requests for access token are made
-    //Given a lapsed GoogleAuthoization
-
 }
 // ReSharper restore InconsistentNaming
