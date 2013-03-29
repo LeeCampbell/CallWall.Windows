833d9a06bb78d655b3767abfe81af5c51d381e50
Lee Campbell <Lee.Ryan.Campbell@gmail.com>
2013-03-29 17:45:37 +0000

    Creating the CollectionChanged sample
    Sample for observing change events from an ObservableCollection<T>, a
    ReadOnlyObservableCollection<T> or items within one of these
    collections.

--- src/CallWall.Core/CollectionChangedData.cs
+++ src/CallWall.Core/CollectionChangedData.cs
@@ -10,6 +10,7 @@ namespace CallWall
         private readonly NotifyCollectionChangedAction _action;
         private readonly ReadOnlyCollection<T> _newItems;
         private readonly ReadOnlyCollection<T> _oldItems;
+        private static readonly T[] _empty = new T[]{};
 
         public CollectionChangedData(NotifyCollectionChangedEventArgs collectionChangedEventArgs) :
             this(collectionChangedEventArgs.OldItems, collectionChangedEventArgs.NewItems)
@@ -20,18 +21,18 @@ namespace CallWall
         public CollectionChangedData(T changedItem)
         {
             _action = NotifyCollectionChangedAction.Replace;
-            _newItems = new ReadOnlyCollection<T>(new T[] { changedItem });
-            _oldItems = new ReadOnlyCollection<T>(new T[] { });
+            _newItems = new ReadOnlyCollection<T>(new[] { changedItem });
+            _oldItems = new ReadOnlyCollection<T>(_empty);
         }
 
         public CollectionChangedData(IEnumerable oldItems, IEnumerable newItems)
         {
             _newItems = newItems == null
-                            ? new ReadOnlyCollection<T>(new T[] { })
+                            ? new ReadOnlyCollection<T>(_empty)
                             : new ReadOnlyCollection<T>(newItems.Cast<T>().ToList());
 
             _oldItems = oldItems == null
-                            ? new ReadOnlyCollection<T>(new T[] { })
+                            ? new ReadOnlyCollection<T>(_empty)
                             : new ReadOnlyCollection<T>(oldItems.Cast<T>().ToList());
 
             _action = _newItems.Count == 0
