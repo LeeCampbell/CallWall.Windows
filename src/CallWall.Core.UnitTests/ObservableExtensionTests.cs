a6e843f3117110661bdcdf9dc4f47612e9921891
Lee Campbell <Lee.Ryan.Campbell@gmail.com>
2013-04-05 00:32:28 +0100

    Adding tests for Stream.ToObservable()
    Also required correcting the implementation. Begin/End (APM) seemed to
    have non-deterministic timing which made for non-deterministic tests.
    Task seems fine however.

---
+++ src/CallWall.Core.UnitTests/ObservableExtensionTests.cs
@@ -0,0 +1,68 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reactive;
+using Microsoft.Reactive.Testing;
+using NUnit.Framework;
+
+// ReSharper disable InconsistentNaming
+namespace CallWall.Core.UnitTests
+{
+    public abstract class Given_a_Stream_of_data
+    {
+        private byte[] _expected;
+        private Stream _stream;
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+            _expected = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            _stream = new MemoryStream(_expected);
+        }
+
+        [TestFixture]
+        public class When_converted_to_an_observable_sequence_of_bytes : Given_a_Stream_of_data
+        {
+            [Test]
+            public void Should_return_all_bytes_in_same_order([Range(1,20)]int bufferSize)
+            {
+                var scheduler = new TestScheduler();
+                var observer = scheduler.CreateObserver<byte>();
+                using (_stream.ToObservable(bufferSize, scheduler).Subscribe(observer))
+                {
+                    scheduler.Start();
+
+                    var actual = observer.Messages.Select(rn => rn.Value)
+                        .Where(n => n.Kind == NotificationKind.OnNext)
+                        .Select(n => n.Value)
+                        .ToList();
+                    CollectionAssert.AreEquivalent(_expected, actual);
+                }
+            }
+
+            [Test]
+            public void Should_throw_is_Source_is_null()
+            {
+                _stream = null;
+                var ex = Assert.Throws<System.ArgumentNullException>(() => _stream.ToObservable(1, new TestScheduler()));
+                Assert.AreEqual("source", ex.ParamName);
+            }
+
+            [Test]
+            public void Should_reject_buffer_of_zero()
+            {
+                var scheduler = new TestScheduler();
+                var ex= Assert.Throws<System.ArgumentOutOfRangeException>(() => _stream.ToObservable(0, scheduler));
+                Assert.AreEqual("bufferSize", ex.ParamName);
+            }
+
+            [Test]
+            public void Should_throw_is_Scheduler_is_null()
+            {
+                var ex = Assert.Throws<System.ArgumentNullException>(() => _stream.ToObservable(1, null));
+                Assert.AreEqual("scheduler", ex.ParamName);
+            }
+        }
+    }
+}
+
+// ReSharper restore InconsistentNaming
--- src/CallWall.Core/ObservableExtensions.cs
+++ src/CallWall.Core/ObservableExtensions.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Reactive.Concurrency;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
 
 namespace CallWall
 {
@@ -45,26 +46,29 @@ namespace CallWall
             int bufferSize,
             IScheduler scheduler)
         {
+            if (source == null) throw new ArgumentNullException("source");
+            if (bufferSize < 1) throw new ArgumentOutOfRangeException("bufferSize");
+            if (scheduler == null) throw new ArgumentNullException("scheduler");
+
             var bytes = Observable.Create<byte>(o =>
                 {
                     var initialState = new StreamReaderState(source, bufferSize);
                     var currentStateSubscription = new SerialDisposable();
                     Action<StreamReaderState, Action<StreamReaderState>> iterator =
                         (state, self) =>
-                        currentStateSubscription.Disposable = state.ReadNext()
-                                                                    .Subscribe(
-                                                                        bytesRead =>
-                                                                            {
-                                                                                for (int i = 0; i < bytesRead; i++)
-                                                                                {
-                                                                                    o.OnNext(state.Buffer[i]);
-                                                                                }
-                                                                                if (bytesRead > 0)
-                                                                                    self(state);
-                                                                                else
-                                                                                    o.OnCompleted();
-                                                                            },
-                                                                        o.OnError);
+                        currentStateSubscription.Disposable = state.ReadNext().Subscribe(
+                            bytesRead =>
+                            {
+                                for (int i = 0; i < bytesRead; i++)
+                                {
+                                    o.OnNext(state.Buffer[i]);
+                                }
+                                if (bytesRead > 0)
+                                    self(state);
+                                else
+                                    o.OnCompleted();
+                            },
+                            o.OnError);
                     var scheduledWork = scheduler.Schedule(initialState, iterator);
                     return new CompositeDisposable(currentStateSubscription, scheduledWork);
                 });
@@ -73,21 +77,19 @@ namespace CallWall
 
         private sealed class StreamReaderState
         {
+            private readonly Stream _source;
             private readonly int _bufferSize;
-            private readonly Func<byte[], int, int, IObservable<int>> _factory;
             public StreamReaderState(Stream source, int bufferSize)
             {
+                _source = source;
                 _bufferSize = bufferSize;
-                _factory = Observable.FromAsyncPattern<byte[], int, int, int>(
-                    source.BeginRead,
-                    source.EndRead);
                 Buffer = new byte[bufferSize];
             }
             public IObservable<int> ReadNext()
             {
-                return _factory(Buffer, 0, _bufferSize);
+                return _source.ReadAsync(Buffer, 0, _bufferSize).ToObservable();
             }
-            public byte[] Buffer { get; set; }
+            public byte[] Buffer { get; private set; }
         }
     }
 }
