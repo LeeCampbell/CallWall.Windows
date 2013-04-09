03f824e3b62c9b9cc0e370af33e1c731ddd68604
Lee Campbell <Lee.Ryan.Campbell@gmail.com>
2013-04-09 23:09:57 +0100

    Refuctor of GrayscalConverter
    Reunderstanding the Bitmap Grayscale converter. Preparing for unit
    testing and potentially covering more PixelFormats

---
+++ src/CallWall.Core.UnitTests/GrayscaleConverterTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using Moq;
+using NUnit.Framework;
+
+namespace CallWall.Core.UnitTests
+{
+    public abstract class Given_a_GrayscaleConverter
+    {
+        private Given_a_GrayscaleConverter()
+        {}
+
+        private GrayscaleConverter _sut;
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+            _sut = new GrayscaleConverter();
+        }
+
+
+        [TestFixture]
+        public sealed class When_converting_a_BitmapSource : Given_a_GrayscaleConverter
+        {
+            private Mock<BitmapSource> _bitmapSourceMock;
+
+            public override void SetUp()
+            {
+                base.SetUp();
+                _bitmapSourceMock = new Mock<BitmapSource>();
+            }
+
+
+        }
+        //TODO: Test various dimensions. Should really test for CallWall values up to 512x512 (the biggest Icon)
+
+        //TODO: Test various dpi. Probably should not just hard code 96x96
+
+        /*TODO: Test each of the c
+            Default: for situations when the pixel format may not be important
+            Indexed1: Paletted image with 2 colors. 
+            Indexed2: Paletted image with 4 colors. 
+            Indexed4: Paletted image with 16 colors.
+            Indexed8: Paletted image with 256 colors.
+            BlackWhite: Monochrome, 2-color image, black and white only.
+            Gray2: Image with 4 shades of gray
+            Gray4: Image with 16 shades of gray 
+            Gray8: Image with 256 shades of gray 
+            Bgr555: 16 bpp SRGB format
+            Bgr565: 16 bpp SRGB format 
+            Rgb128Float: 128 bpp extended format; Gamma is 1.0 
+            Bgr24: 24 bpp SRGB format
+            Rgb24: 24 bpp SRGB format
+            Bgr101010: 32 bpp SRGB format
+            Bgr32: 32 bpp SRGB format
+            Bgra32: 32 bpp SRGB format 
+            Pbgra32: 32 bpp SRGB format 
+            Rgb48: 48 bpp extended format; Gamma is 1.0
+            Rgba64: 64 bpp extended format; Gamma is 1.0 
+            Prgba64: 64 bpp extended format; Gamma is 1.0 
+            Gray16: 16 bpp Gray-scale format; Gamma is 1.0
+            Gray32Float: 32 bpp Gray-scale format; Gamma is 1.0
+            Rgba128Float: 128 bpp extended format; Gamma is 1.0
+            Prgba128Float: 128 bpp extended format; Gamma is 1.0
+            Cmyk32: 32 bpp format 
+         */
+    }
+}
--- src/CallWall.Core/GrayScaleConverter.cs
+++ src/CallWall.Core/GrayScaleConverter.cs
@@ -25,29 +25,8 @@ namespace CallWall
             }
             if (bitmapSource != null)
             {
-                BitmapSource orgBmp = bitmapSource;
-                if (orgBmp.Format == PixelFormats.Bgra32 ||orgBmp.Format == PixelFormats.Bgr32)
-                {
-                    byte[] orgPixels = new byte[orgBmp.PixelHeight *
-                                                orgBmp.PixelWidth * 4];
-                    byte[] newPixels = new byte[orgPixels.Length];
-                    orgBmp.CopyPixels(orgPixels, orgBmp.PixelWidth * 4, 0);
-                    for (int i = 3; i < orgPixels.Length; i += 4)
-                    {
-                        int grayVal = ((int)orgPixels[i - 3] +
-                                       (int)orgPixels[i - 2] + (int)orgPixels[i - 1]);
-
-                        if (grayVal != 0)
-                            grayVal = grayVal / 3;
-                        newPixels[i] = orgPixels[i]; //Set AlphaChannel
-                        newPixels[i - 3] = (byte)grayVal;
-                        newPixels[i - 2] = (byte)grayVal;
-                        newPixels[i - 1] = (byte)grayVal;
-                    }
-                    return BitmapSource.Create(orgBmp.PixelWidth, orgBmp.PixelHeight,
-                                               96, 96, PixelFormats.Bgra32, null, newPixels,
-                                               orgBmp.PixelWidth * 4);
-                }
+                var converter = GetConverter(bitmapSource.Format);
+                return converter.Convert(bitmapSource);
             }
             return value;
         }
@@ -55,6 +34,66 @@ namespace CallWall
         object IValueConverter.ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             throw new NotSupportedException();
+        }
+
+        public static IBitmapSourceGrayScaleConverter GetConverter(PixelFormat format)
+        {
+            if (format == PixelFormats.Bgra32 || format == PixelFormats.Bgr32)
+                return BgrGrayScaleConverter.Instance;
+
+
+            throw new NotSupportedException(string.Format("PixelFormat {0} is not supported", format.GetType().Name));
+        }
+
+        public static byte ToGray(byte red, byte green, byte blue)
+        {
+            int grayVal = (red + green + blue);
+
+            if (grayVal != 0)
+                grayVal = grayVal / 3;
+            return (byte)grayVal;
+        }
+    }
+
+    public interface IBitmapSourceGrayScaleConverter
+    {
+        BitmapSource Convert(BitmapSource source);
+    }
+
+    public sealed class BgrGrayScaleConverter : IBitmapSourceGrayScaleConverter
+    {
+        private const int Depth = 4;
+        //private const int DpiX = 96;  //Maintain the source DPI
+        //private const int DpiY = 96;
+        private static readonly BitmapPalette _bitmapPalette = null;    //Only useful for index palettes.
+
+        private BgrGrayScaleConverter()
+        {}
+
+        public static readonly IBitmapSourceGrayScaleConverter Instance = new BgrGrayScaleConverter();
+
+        public BitmapSource Convert(BitmapSource source)
+        {
+            var orgPixels = new byte[source.PixelHeight * source.PixelWidth * Depth];
+            var newPixels = new byte[orgPixels.Length];
+            source.CopyPixels(orgPixels, source.PixelWidth * Depth, 0);
+            for (int i = 3; i < orgPixels.Length; i += Depth)
+            {
+                var grayVal = GrayscaleConverter.ToGray(orgPixels[i - 3], orgPixels[i - 2], orgPixels[i - 1]);
+
+                newPixels[i - 3] = grayVal;
+                newPixels[i - 2] = grayVal;
+                newPixels[i - 1] = grayVal;
+                newPixels[i] = orgPixels[i]; //Set AlphaChannel
+            }
+            return BitmapSource.Create(source.PixelWidth, source.PixelHeight,
+                                       source.DpiX, source.DpiY, //Not sure why we don't do this?
+                                       //DpiX, DpiY, 
+                                       PixelFormats.Bgra32, //} There seems to be no built in support for 256 gray values with 256alpha. Instead you
+                                       _bitmapPalette,      //}     have to recreate the gray with RGB and maintain the alpha from the source.
+                                       newPixels,
+                                       source.PixelWidth * Depth);
+
         }
     }
 }
