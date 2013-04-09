5bc04cd5fd6575fb0cbd7cb4afd8e755ec8a2669
Lee Campbell <Lee.Ryan.Campbell@gmail.com>
2013-04-09 19:28:02 +0100

    Adding BooleanToInvisibilityConverter unit tests

--- src/CallWall.Core/BooleanToInvisibilityConverter.cs
+++ src/CallWall.Core/BooleanToInvisibilityConverter.cs
@@ -12,18 +12,13 @@ namespace CallWall
             var flag = false;
             if (value is bool)
                 flag = (bool)value;
-            else if (value is bool?)
-            {
-                bool? nullable = (bool?)value;
-                flag = nullable.HasValue && nullable.Value;
-            }
             return flag ? Visibility.Collapsed : Visibility.Visible;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is Visibility)
-                return (Visibility)value != Visibility.Visible;
+                return (Visibility)value == Visibility.Collapsed;
             return false;
         }
     }
