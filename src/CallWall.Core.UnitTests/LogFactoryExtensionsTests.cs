4cd0b1bce4f310a7a771a6f1da6417943da55e7e
Lee Campbell <Lee.Ryan.Campbell@gmail.com>
2013-04-15 19:46:16 +0100

    Added LogFactoryExtensions unit test

---
+++ src/CallWall.Core.UnitTests/LogFactoryExtensionsTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace CallWall.Core.UnitTests
+{
+    public abstract class Given_an_ILoggerFactory_instance
+    {
+        private Mock<ILoggerFactory> _loggerFactoryMock;
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+            _loggerFactoryMock = new Mock<ILoggerFactory>();
+        }
+
+        public class When_calling_CreateLogger_with_no_params : Given_an_ILoggerFactory_instance
+        {
+            [Test]
+            public void Should_pass_type_that_calling_method_belongs_to()
+            {
+                var expected = this.GetType();
+                var logger = new Mock<ILogger>().Object;
+                _loggerFactoryMock.Setup(lf => lf.CreateLogger(expected))
+                    .Returns(logger);
+
+                var actual = _loggerFactoryMock.Object.CreateLogger();
+
+                Assert.AreEqual(logger, actual);
+                _loggerFactoryMock.Verify();
+            }
+        }
+    }
+}
