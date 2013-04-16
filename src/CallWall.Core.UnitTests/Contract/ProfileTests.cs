d43c5df3002b07d2b017591a0c201b82316b6667
Lee Campbell <Lee.Ryan.Campbell@gmail.com>
2013-04-16 21:54:28 +0100

    Added Profile Unit Tests

---
+++ src/CallWall.Core.UnitTests/Contract/ProfileTests.cs
@@ -0,0 +1,29 @@
+﻿using CallWall.Contract;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+// ReSharper disable InconsistentNaming
+namespace CallWall.Core.UnitTests.Contract
+{
+    [TestFixture]
+    public class ProfileTests
+    {
+        [Test]
+        public void Should_return_values_to_ctor_from_Identifiers_property()
+        {
+            var value1 = CreatePersonalIdentifier();
+            var value2 = CreatePersonalIdentifier();
+            IEnumerable<IPersonalIdentifier> values = new[] { value1, value2 };
+            var sut = new Profile(values);
+
+            CollectionAssert.AreEqual(values, sut.Identifiers);
+        }
+
+        private static IPersonalIdentifier CreatePersonalIdentifier()
+        {
+            return new Mock<IPersonalIdentifier>().Object;
+        }
+    }
+}
+// ReSharper restore InconsistentNaming
