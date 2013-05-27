86acff97304326d39d977e62850a2f9f268cbd2a
Lee Campbell <Lee.Ryan.Campbell@gmail.com>
2013-05-27 18:46:59 +0100

    Added OAuthUriFactory tests

--- src/CallWall.Google/Authorization/OAuthUriFactory.cs
+++ src/CallWall.Google/Authorization/OAuthUriFactory.cs
@@ -7,21 +7,23 @@ namespace CallWall.Google.Authorization
 {
     public sealed class OAuthUriFactory : IOAuthUriFactory
     {
-        private const string ClientId = "410654176090.apps.googleusercontent.com";  //}
-        private const string ClientSecret = "bDkwW8Y2RnUt0JsjbAwYA8cb";             //} TODO:This is all the Spike stuff. Might need to change.
-        private const string RedirectUri = "urn:ietf:wg:oauth:2.0:oob";             //} I probably will eventually change this to be related to a CallWall.com email/google address.
+        //HACK:Potentially this should be retrieved from config so it can not be decompiled. -LC
+        private const string ClientId = "410654176090.apps.googleusercontent.com";  //} TODO:This is all the Spike stuff. Might need to change.
+        //HACK:Potentially this should be retrieved from config so it can not be decompiled. -LC
+        private const string ClientSecret = "bDkwW8Y2RnUt0JsjbAwYA8cb";             //} I probably will eventually change this to be related to a CallWall.com email/google address.
+        private const string RedirectUri = "urn:ietf:wg:oauth:2.0:oob";
 
         public Uri BuildAuthorizationUri(IEnumerable<Uri> requestedResources)
         {
             var queryString = System.Web.HttpUtility.ParseQueryString(string.Empty);
             queryString["response_type"] = "code";
-            queryString["client_id"] = ClientId; //Lee.Ryan.Campbell@gmail.com client Id
+            queryString["client_id"] = ClientId;                                    //Lee.Ryan.Campbell@gmail.com client Id
             queryString["redirect_uri"] = RedirectUri;
             var scopes = requestedResources.Select(uri => uri.ToString()).ToArray();
-            queryString["scope"] = string.Join(" ", scopes);    //" " should be translated into a "+" as per https://developers.google.com/accounts/docs/OAuth2InstalledApp
+            queryString["scope"] = string.Join(" ", scopes);                        //" " should be translated into a "+" as per https://developers.google.com/accounts/docs/OAuth2InstalledApp
 
             var authorizationUri = new UriBuilder(@"https://accounts.google.com/o/oauth2/auth");
-            authorizationUri.Query = queryString.ToString(); // Returns "key1=value1&key2=value2", all URL-encoded
+            authorizationUri.Query = queryString.ToString();                        // Returns "key1=value1&key2=value2", all URL-encoded
             return authorizationUri.Uri;
         }
 
@@ -38,7 +40,7 @@ namespace CallWall.Google.Authorization
             return requestParams;
         }
 
-        public  HttpRequestParameters CreateRefreshTokenWebRequest(string refreshToken)
+        public HttpRequestParameters CreateRefreshTokenWebRequest(string refreshToken)
         {
             var requestParams = new HttpRequestParameters(@"https://accounts.google.com/o/oauth2/token");
             requestParams.PostParameters.Add("client_id", ClientId);
