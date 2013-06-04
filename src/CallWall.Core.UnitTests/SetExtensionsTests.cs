366a165dcdf57d89f8d1921bca845a66fe9cc292
Lee Campbell <Lee.ryan.campbell@gmail.com>
2013-06-04 18:34:23 +0100

    Add SetExtensions tests

---
+++ src/CallWall.Core.UnitTests/SetExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace CallWall.Core.UnitTests
+{
+    [TestFixture]
+    public sealed class SetExtensionsTests
+    {
+        [Test]
+        public void AddRange_should_add_provided_values_to_target_Set()
+        {
+            var target = new HashSet<int>();    
+            var source = new[] {1, 2, 3, 4, 5};
+            target.AddRange(source);
+
+            CollectionAssert.AreEquivalent(source, target);
+        }
+    }
+}
