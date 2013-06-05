9dfdcd1f8aa04a74544919245e7bdd20734ce917
Lee Campbell <Lee.ryan.campbell@gmail.com>
2013-06-05 11:03:04 +0100

    Correcting CRLF in Json tests

--- src/CallWall.UnitTests/JsonSerializerFixture.cs
+++ src/CallWall.UnitTests/JsonSerializerFixture.cs
@@ -9,6 +9,7 @@ namespace CallWall.UnitTests
         [Test]
         public void Can_serialize_to_Json_format()
         {
+            var expected = "{\r\n  \"DateOfBirth\": \"1980-06-15T00:00:00\",\r\n  \"Name\": \"Fred\",\r\n  \"Rank\": 3\r\n}";
             var expectedRank = 3;
             var expectedName = "Fred";
             var expectedDOB = new DateTime(1980, 6, 15);
@@ -18,12 +19,7 @@ namespace CallWall.UnitTests
                 Name = expectedName,
                 DateOfBirth = expectedDOB
             };
-            var expected = @"{
-  ""DateOfBirth"": ""1980-06-15T00:00:00"",
-  ""Name"": ""Fred"",
-  ""Rank"": 3
-}";
-
+            
             var sut = new JsonSerializer();
             var payload = sut.Serialize(data);
             Assert.AreEqual(expected, payload);
@@ -32,11 +28,7 @@ namespace CallWall.UnitTests
         [Test]
         public void Can_Deserialize_from_Json_format()
         {
-            var source = @"{
-  ""DateOfBirth"": ""1980-06-15T00:00:00"",
-  ""Name"": ""Fred"",
-  ""Rank"": 3
-}";
+            var source = "{\r\n  \"DateOfBirth\": \"1980-06-15T00:00:00\",\r\n  \"Name\": \"Fred\",\r\n  \"Rank\": 3\r\n}";
             var sut = new JsonSerializer();
             var actual = sut.Deserialize<Person>(source);
             Assert.AreEqual(3, actual.Rank);
