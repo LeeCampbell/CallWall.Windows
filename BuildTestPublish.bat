f53a190beed4110712162489f1efa8a1944f9e82
Lee Campbell <Lee.ryan.campbell@gmail.com>
2013-06-25 18:53:50 +0100

    WIP. Preparing clickonce. Removed unused icon

--- BuildTestPublish.bat
+++ BuildTestPublish.bat
@@ -1,10 +1,20 @@
 @ECHO OFF
 
-MSBuild CallWall.Windows.build
+MSBuild CallWall.Windows.build /p:BuildVersion=0.0.0.2
 IF %ERRORLEVEL% NEQ 0 GOTO :ERROR
 
-NUnit-console .\bin\Test\CallWall.UnitTests.dll .\bin\Test\CallWall.Core.UnitTests.dll
+ECHO "NUnit not part of tool set"
+REM NUnit-console .\bin\Test\CallWall.UnitTests.dll .\bin\Test\CallWall.Core.UnitTests.dll
 
+ECHO "Creating application manifest"
+REM This works
+cd .\bin\release
+Mage -New Application -ToFile CallWall.manifest -FromDirectory . -Version 0.0.0.1  -IconFile CallWall.ico -Name "CallWall (Local)"
+
+REM These fail
+REM		Mage -New Application -ToFile CallWall.manifest -Algorithm sha256RSA -FromDirectory bin/release	-IconFile CallWall.ico -TrustLevel "Internet" -Version 0.0.0.1
+REM		Mage -New Application -ToFile CallWall.manifest -FromDirectory bin/release -IconFile CallWall.ico -TrustLevel "Internet" -Version 0.0.0.1
+REM			Mage -New Application -ToFile CallWall.manifest -FromDirectory bin/release -Version 0.0.0.1 -TrustLevel "Internet"
 
 EXIT /B 0
 
