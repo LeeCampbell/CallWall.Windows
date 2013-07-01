7f1dfbb4cb5b48af4a7ea1d5ae0acbdf4cb56b47
Lee Campbell <Lee.ryan.campbell@gmail.com>
2013-07-01 08:19:33 +0100

    Removing the harding of color ramp in Welcome header converter

--- src/CallWall.Shell/Welcome/WelcomeSettingStepBackgroundConverter.cs
+++ src/CallWall.Shell/Welcome/WelcomeSettingStepBackgroundConverter.cs
@@ -1,26 +1,40 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Media;
 
 namespace CallWall.Welcome
 {
     public sealed class WelcomeSettingStepBackgroundConverter : IValueConverter
     {
-        //TODO: Move this to a publicly settable collection
-        private static readonly string[] _colorMap = new[] { "#806080", "#804080", "#802080", "#800080", };
+        private readonly BrushCollection _colorRamp = new BrushCollection();
+
+        public BrushCollection ColorRamp
+        {
+            get { return _colorRamp; }
+        }
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var accoridion = value as AccordionItem;
             if (accoridion == null) return string.Empty;
 
             var index = accoridion.IndexInParent<Accordion>();
-            return _colorMap[index];
+            return ColorRamp[index];
         }
 
         object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotSupportedException();
+        }
+
+
+        public sealed class BrushCollection
+        : List<Brush>
+        {
+
         }
     }
 }
