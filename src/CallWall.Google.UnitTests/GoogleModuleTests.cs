82efedbd34953e3dbcd8458f07393952d201845b
Lee Campbell <Lee.ryan.campbell@gmail.com>
2013-11-06 17:59:21 +0000

    Removed unity from Google tests

--- src/CallWall.Google.UnitTests/GoogleModuleTests.cs
+++ src/CallWall.Google.UnitTests/GoogleModuleTests.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Linq;
-using CallWall.Contract;
+﻿using CallWall.Contract;
 using CallWall.Contract.Contact;
 using CallWall.Google.AccountConfiguration;
 using CallWall.Google.Authorization;
 using CallWall.Google.Authorization.Login;
 using CallWall.Google.Providers.Contacts;
-using CallWall.Testing;
-using Microsoft.Practices.Unity;
 using Moq;
 using NUnit.Framework;
 
-// ReSharper disable ReplaceWithSingleCallToSingle
 // ReSharper disable InconsistentNaming
 namespace CallWall.Google.UnitTests
 {
@@ -41,7 +36,6 @@ namespace CallWall.Google.UnitTests
         [TestFixture]
         public sealed class When_Initialised : Given_a_constructed_GoogleModule
         {
-           
             public override void SetUp()
             {
                 base.SetUp();
@@ -52,87 +46,45 @@ namespace CallWall.Google.UnitTests
             [Test]
             public void Should_register_GoogleAccountConfiguration_instance()
             {
-                //_containerMock.Verify(c => c.RegisterType(typeof(IAccountConfiguration), typeof(GoogleAccountConfiguration), "GoogleAccountConfiguration", It.IsAny<ContainerControlledLifetimeManager>()), Times.Once());
                 _containerMock.Verify(c => c.RegisterCompositeAsSingleton<IAccountConfiguration,GoogleAccountConfiguration>(), Times.Once());
             }
 
             [Test]
             public void Should_register_GoogleAuthorization_instance()
             {
-                //_containerMock.Verify(c => c.RegisterType(typeof(IGoogleAuthorization), typeof(GoogleAuthorization), (string)null, It.IsAny<ContainerControlledLifetimeManager>()), Times.Once());
                 _containerMock.Verify(c => c.RegisterTypeAsSingleton<IGoogleAuthorization,GoogleAuthorization>(), Times.Once());
             }
 
             [Test]
             public void Should_register_GoogleLoginView_instance()
             {
-                //_containerMock.Verify(c => c.RegisterType(typeof(IGoogleLoginView), typeof(GoogleLoginView), (string)null, It.IsAny<ContainerControlledLifetimeManager>()), Times.Once());
                 _containerMock.Verify(c => c.RegisterTypeAsSingleton<IGoogleLoginView,GoogleLoginView>(), Times.Once());
             }
+            
             [Test]
             public void Should_register_LoginController_instance()
             {
-                //_containerMock.Verify(c => c.RegisterType(typeof(ILoginController), typeof(LoginController), (string)null, It.IsAny<ContainerControlledLifetimeManager>()), Times.Once());
                 _containerMock.Verify(c => c.RegisterTypeAsSingleton<ILoginController,LoginController>(), Times.Once());
             }
 
             [Test]
             public void Should_register_GoogleAccountSetupView_instance()
             {
-                //_containerMock.Verify(c => c.RegisterType(typeof(IGoogleAccountSetupView), typeof(GoogleAccountSetupView), (string)null, It.IsAny<ContainerControlledLifetimeManager>()), Times.Once());
                 _containerMock.Verify(c => c.RegisterTypeAsSingleton<IGoogleAccountSetupView,GoogleAccountSetupView>(), Times.Once());
             }
 
             [Test]
             public void Should_register_GoogleAccountSetupViewModel_instance()
             {
-                //_containerMock.Verify(c => c.RegisterType(typeof(IGoogleAccountSetupViewModel), typeof(GoogleAccountSetupViewModel), (string)null, It.IsAny<ContainerControlledLifetimeManager>()), Times.Once());
                 _containerMock.Verify(c => c.RegisterTypeAsSingleton<IGoogleAccountSetupViewModel,GoogleAccountSetupViewModel>(), Times.Once());
             }
             
-            /*
-                This is a strategy for registering a singleton but as two separate types. This maintains lazy instantiation.
-             
-             */
             [Test]
-            public void Should_register_GoogleContactQueryProvider_to_container_as_IGoogleContactQueryProvider()
+            public void Should_register_GoogleContactQueryProvider_to_container_as_IGoogleContactQueryProvider_and_IContactQueryProvider()
             {
-                //var container = new StubUnityContainer();
-                //var loginControllerMock = new Mock<ILoginController>();
-                //container.RegisterInstance(typeof(ILoginController), loginControllerMock.Object);
-
-                //var sut = new GoogleModule(container);
-
-                //sut.Initialize();
-                //container.RegisteredTypes
-                //    .Where(rt => rt.From == typeof(IGoogleContactQueryProvider))
-                //    .Where(rt => rt.To == typeof(GoogleContactQueryProvider))
-                //    .Where(rt => rt.Name == null)
-                //    .Where(rt => rt.LifetimeManager is ContainerControlledLifetimeManager)
-                //    .Single();
-
                 _containerMock.Verify(c => c.RegisterCompositeAsSingleton<IContactQueryProvider,IGoogleContactQueryProvider, GoogleContactQueryProvider>(), Times.Once());
             }
-            //[Test]
-            //public void Should_register_GoogleContactQueryProvider_to_container_as_named_IContactQueryProvider()
-            //{
-            //    var container = new StubUnityContainer();
-            //    var loginControllerMock = new Mock<ILoginController>();
-            //    container.RegisterInstance(typeof (ILoginController), loginControllerMock.Object);
-
-
-            //    var sut = new GoogleModule(container);
-            //    sut.Initialize();
-            //    container.RegisteredTypes
-            //        .Where(rt => rt.From == typeof(IContactQueryProvider))
-            //        .Where(rt => rt.To == typeof(GoogleContactQueryProvider))
-            //        .Where(rt => rt.Name == "GoogleContactQueryProvider")
-            //        .Where(rt => rt.LifetimeManager is ExternallyControlledLifetimeManager) //} HACK: A poor way of checking if the instance is registered
-            //        .Where(rt => rt.InjectionMembers.Single() is InjectionFactory)          //} to actually redirect to the registered IDemoActivatedIdentityListener instance/type.
-            //        .Single();
-            //}
             
-
             [Test]
             public void Should_start_LoginController()
             {
@@ -142,4 +94,3 @@ namespace CallWall.Google.UnitTests
     }
 }
 // ReSharper restore InconsistentNaming
-// ReSharper restore ReplaceWithSingleCallToSingle
